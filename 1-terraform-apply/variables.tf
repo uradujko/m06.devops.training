690a8a6e01a4ede9e8af1a14128c8a10add05106
Uros Radujko <uradujko@RTRKN1389-LIN.domain.local>
2026-05-11 09:40:20 +0200

    Update bucket name

--- 1-terraform-apply/variables.tf
+++ 1-terraform-apply/variables.tf
@@ -23,6 +23,6 @@ variable "aws_endpoint" {
 variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
-  default     = "my-test-bucket"
+  default     = "group1"
 }
 
