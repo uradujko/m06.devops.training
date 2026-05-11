4092b1a270f6bee6665e493d04802d5bfe8b835b
Uros Radujko <uradujko@RTRKN1389-LIN.domain.local>
2026-05-11 09:42:58 +0200

    run terraform

--- 1-terraform-apply/variables.tf
+++ 1-terraform-apply/variables.tf
@@ -23,6 +23,6 @@ variable "aws_endpoint" {
 variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
-  default     = "group1"
+  default     = "group1-uros"
 }
 
